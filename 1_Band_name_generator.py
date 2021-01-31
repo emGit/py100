20d818afa9ebd5bc8530be4221d449de42fd7c60
emGit <41939170+emGit@users.noreply.github.com>
2021-01-31 21:48:36 +0800

    Update 1_Band_name_generator.py

--- 1_Band_name_generator.py
+++ 1_Band_name_generator.py
@@ -1,28 +1,11 @@
-#1. Create a greeting for your program.
 name = input("What's your name? \n")
-
 print(f'\nHello, {name}!')
-
-#2. Ask the user for the city that they grew up in.
 city = input(f'\nSo, {name}, where did you grow up? \n').lower()
+pets = input('\nDo you have any pets? \n').lower()
 
-#3. Ask the user for the name of a pet.
-
-pets = input('\nDo you have any pets? \n')
-
-if(pets == "yes" or pets == "Yes"):
-  pet_name = input("\nWhat is your pet's name?\n")
+if pets[0]=="y": pet_name = input("\nWhat is your pet's name?\n")
 else:
   print("\nAh, I see.")
-  animal_array = ["Cosmo", "Fluff", "Korra", "Wiggles", "Roach"]
   import random
-  pet_name = random.choice(animal_array)
-
-#4. Combine the name of their city and pet and show them their band name.
-
+  pet_name = random.choice(["Cosmo", "Fluff", "Korra", "Wiggles", "Roach"])
 print(f'\nI think {pet_name}{city} would make a sick band name for you.')
-
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://band-name-generator-end.appbrewery.repl.run/
-
-#input(prompting text), len(variable), variable.lower()
